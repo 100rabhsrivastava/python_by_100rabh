0f24a32b929f24ca5d6140c51207e6e8357b0109
saurabh srivastava <srivastava100rabh1142000@gmail.com>
2021-03-20 22:35:01 +0530

    --- Auto-Git Commit ---

---
+++ pp21.py
@@ -0,0 +1,14 @@
+# def num(n):
+    
+for i in name:
+    s = 0
+    if (5==i):
+         s +=s
+    print(s)
+return(s)
+        
+    
+
+name = [1,2,3,4,5,6,7,8,9,15,3,5,5,5,5,5,5,2,1,2,3,4,5,]
+# value =  num(b)
+# print("vlue is" ,value)
