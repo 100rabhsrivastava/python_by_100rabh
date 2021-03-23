f97d827aab3d30e09430ea368cf3a0b32173a504
saurabh srivastava <srivastava100rabh1142000@gmail.com>
2021-03-23 22:37:37 +0530

    --- Auto-Git Commit ---

---
+++ pp24.py
@@ -0,0 +1,7 @@
+for i in range(8):
+    for k in range(i):
+        print(k , end="")
+    for j in range(8-i):
+        print("! " , end="")
+
+    print()
---
+++ pp25.py
@@ -0,0 +1,5 @@
+c= 'H'
+width = 9
+
+for i  in range(7):
+    print((c*i).center(width ,' '))
