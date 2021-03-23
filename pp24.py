18eeea12892b1874ec87d4d9b92f5f11d21569a0
saurabh srivastava <srivastava100rabh1142000@gmail.com>
2021-03-23 23:09:35 +0530

    --- Auto-Git Commit ---

--- pp24.py
+++ pp24.py
@@ -1,7 +1,7 @@
-for i in range(8):
+for i in range(10):
     for k in range(i):
-        print(k , end="")
-    for j in range(8-i):
-        print("! " , end="")
+        print((i-k) , end="")
+    # for j in range(8-i):
+    #     print("! " , end="")
 
     print()
--- pp25.py
+++ pp25.py
@@ -1,5 +1,13 @@
-c= 'H'
-width = 9
+rows = 6
 
-for i  in range(7):
-    print((c*i).center(width ,' '))
+for i in range(1, rows + 1):
+    k= i-1
+    for j in range(1, k):
+
+        print(j, end="")
+
+    for j in range(k, 0, -1):
+
+        print(j, end="")
+
+    print()
