45ac87c569f78934cecbc3d2b36a66308cb7aa80
saurabh srivastava <srivastava100rabh1142000@gmail.com>
2021-04-25 22:29:30 +0530

    --- Auto-Git Commit ---

--- pp26.py
+++ pp26.py
@@ -1,7 +1,24 @@
 from array import *
-vals = array( 'i' ,[1,2,3,3,3,4,5,66])
+vals = array( 'i' ,[1,2,3,4,5,6,7,8])
 print(vals)
-for i in range(8):
-    print(vals[i])
+for i in range(3):
+    while(i<7):
+        f = i+1
+        print(vals[f] , end = " ")
+        i = i+1;
+    print("\n")
+# m = number of shift 
+# n = size of array 
+# e =  incremental integer 
+# for e in range (4):
+#     f = 0;   #number of shift 
+#     if (f<(n-1)):
+#          print(vals[i] , end = " ")
+#     else:
+#         statement
+
+print("saurabh");
+    
+    
 
 
